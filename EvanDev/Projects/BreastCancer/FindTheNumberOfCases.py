7e591a48bdd719616adb6b6324664b693e279a8a
ev-h <ev-h>
2024-10-25 14:11:20 +0000

    update numbers

--- EvanDev/Projects/BreastCancer/FindTheNumberOfCases.py
+++ EvanDev/Projects/BreastCancer/FindTheNumberOfCases.py
@@ -241,10 +241,26 @@ display(tmp_df)
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC SELECT DISTINCT ExaminationDescription
+# MAGIC FROM 4_prod.raw.pacs_examinations AS e
+# MAGIC WHERE ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+# MAGIC AND LOWER(ExaminationDescription) LIKE 'mg%' OR LOWER(ExaminationDescription) LIKE '%mammogram%' 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT DISTINCT ExaminationDescription
+# MAGIC FROM 4_prod.raw.pacs_examinations AS e
+# MAGIC WHERE ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+# MAGIC AND LOWER(ExaminationDescription) LIKE 'us%' OR LOWER(ExaminationDescription) LIKE '%ultraso%' OR LOWER(ExaminationDescription) LIKE '%sonograph%' OR LOWER(ExaminationDescription) LIKE '%echo%'
+
+# COMMAND ----------
+
 
 modalities = {
     "mammogram":["%mammogram%", "MG%"],
-    "ultrasound":["%ultrasound%", "US%", "%echo", "%sonograph%"],
+    "ultrasound":["%ultraso%", "US%", "%echo", "%sonograph%"],
     "mri":["% MRI %", 'MR%'],
     "pet":["% pet %", 'PT%'],
     "ct":["ct%"],
@@ -261,7 +277,9 @@ INNER JOIN breast_cancer_patients AS p
 ON e.ExaminationId = p.ExaminationId
 INNER JOIN 4_prod.raw.pacs_examinationreports AS er
 ON e.ExaminationId = er.ExaminationReportExaminationId
-WHERE 
+WHERE
+ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+AND (__COND__)
 """
 
 q_template_cond = """LOWER(e.ExaminationDescription) LIKE LOWER('__VAL__') OR LOWER(e.ExaminationModality) LIKE LOWER('__VAL__')\n"""
@@ -278,7 +296,7 @@ for i, key_val in enumerate(modalities.items()):
         
         q_cond +=q_template_cond.replace("__VAL__", val)
 
-    q += q_template_main.replace("__KEY__", key_val[0])+q_cond
+    q += q_template_main.replace("__KEY__", key_val[0]).replace("__COND__", q_cond)
 
 
 print(q)
@@ -304,8 +322,228 @@ display(tmp_df)
 
 # COMMAND ----------
 
+
+keywords = {
+  "Screening": ["%screen%"],
+  "Diagnostic": ["%diagnostic%"],
+  "Follow-up": ["%follow up%", "%follow-up%", "%F/up%"],
+  "On-treatment": ["%on-treatment%", "%on treatment%", "%on-tx%", "%on tx%"],
+  "Post-treatment": ["%post-treatment%", "%post treatment%", "%post-tx%", "%post tx%"]
+}
+
+q_template_main = """
+SELECT '__KEY__', COUNT(DISTINCT e.ExaminationId)
+FROM 4_prod.raw.pacs_examinations AS e
+INNER JOIN breast_cancer_patients AS p
+ON e.ExaminationId = p.ExaminationId
+INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+ON e.ExaminationId = er.ExaminationReportExaminationId
+LEFT JOIN 4_prod.raw.pacs_requests AS r
+ON e.ExaminationRequestId = r.RequestId
+WHERE 
+ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+AND (__COND__)
+"""
+
+q_template_cond = """LOWER(e.ExaminationDescription) LIKE LOWER('__VAL__') OR LOWER(r.RequestQuestion) LIKE LOWER('__VAL__')\n"""
+
+q = ""
+for i, key_val in enumerate(keywords.items()):
+    if i > 0:
+        q += "\nUNION\n"
+
+    q_cond = ""
+    for j, val in enumerate(key_val[1]):
+        if j > 0:
+            q_cond += " OR "
+        
+        q_cond += q_template_cond.replace("__VAL__", val)
+
+    q += q_template_main.replace("__KEY__", key_val[0]).replace("__COND__", q_cond)
+
+
+print(q)
+
+
+
+
+
+# COMMAND ----------
+
+tmp_df = spark.sql(q)
+
+display(tmp_df)
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC
 # MAGIC SELECT *
 # MAGIC from 4_prod.raw.pacs_Reports
+# MAGIC LIMIT 10
+
+# COMMAND ----------
+
+
+keywords = {
+  "Investigation, Assessment or Planning": ["%investigation%", "%assessment%", "%planning%"],
+  "Family History Screening": ["%family history screen%", "%family hx screen%"],
+  "Follow-up": ["%follow up%", "%follow-up%", "%F/up%"],
+  "High risk": ["high risk%"],
+  "Routine Screening": ["%routine screen%"],
+  "Staging": ["%Staging%"],
+  "Treatment Response": ["%treatment response%", "%tx response%"]
+}
+
+q_template_main = """
+SELECT '__KEY__', COUNT(DISTINCT e.ExaminationId)
+FROM 4_prod.raw.pacs_examinations AS e
+INNER JOIN breast_cancer_patients AS p
+ON e.ExaminationId = p.ExaminationId
+INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+ON e.ExaminationId = er.ExaminationReportExaminationId
+LEFT JOIN 4_prod.raw.pacs_requests AS r
+ON e.ExaminationRequestId = r.RequestId
+WHERE 
+ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+AND (__COND__)
+"""
+
+q_template_cond = """LOWER(e.ExaminationDescription) LIKE LOWER('__VAL__') OR LOWER(r.RequestQuestion) LIKE LOWER('__VAL__')\n"""
+
+q = ""
+for i, key_val in enumerate(keywords.items()):
+    if i > 0:
+        q += "\nUNION\n"
+
+    q_cond = ""
+    for j, val in enumerate(key_val[1]):
+        if j > 0:
+            q_cond += " OR "
+        
+        q_cond += q_template_cond.replace("__VAL__", val)
+
+    q += q_template_main.replace("__KEY__", key_val[0]).replace("__COND__", q_cond)
+
+
+print(q)
+
+
+
+
+
+# COMMAND ----------
+
+tmp_df = spark.sql(q)
+
+display(tmp_df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT * FROM 4_prod.raw.pacs_reports AS r
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+# MAGIC ON r.ReportId = er.ExaminationReportReportId
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinations AS e
+# MAGIC ON er.ExaminationReportExaminationId = e.ExaminationId
+# MAGIC INNER JOIN breast_cancer_patients AS bcp
+# MAGIC ON e.ExaminationPatientId = bcp.PatientId
+# MAGIC LIMIT 10
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT * FROM 4_prod.raw.pacs_reports AS r
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+# MAGIC ON r.ReportId = er.ExaminationReportReportId
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinations AS e
+# MAGIC ON er.ExaminationReportExaminationId = e.ExaminationId
+# MAGIC INNER JOIN breast_cancer_patients AS bcp
+# MAGIC ON e.ExaminationPatientId = bcp.PatientId
+# MAGIC WHERE LOWER(ReportText) LIKE '%partial response%' AND LOWER(ReportText) NOT LIKE '%partial response to treatment%'
+# MAGIC LIMIT 10
+
+# COMMAND ----------
+
+
+keywords = {
+  "Partial": ["%partial response%", "%partial treatment response%", "%partial tx response%"],
+  "Mixed": ["%mixed response%", "%mixed treatment response%", "%mixed tx response%"],
+  "Complete": ["%complete response%", "%complete treatment response%", "%complete tx response%"],
+  "Stable disease": ["%stable disease%"],
+  "Progressive disease": ["%progressive disease%"],
+  "Unknown": ["%unknown response%"]
+}
+
+q_template_main = """
+SELECT '__KEY__', COUNT(DISTINCT rp.reportid)
+FROM 4_prod.raw.pacs_reports AS rp
+INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+ON rp.ReportId = er.ExaminationReportReportId
+INNER JOIN 4_prod.raw.pacs_examinations AS e
+ON e.ExaminationId = er.ExaminationReportExaminationId
+INNER JOIN breast_cancer_patients AS bcp
+ON e.ExaminationId = bcp.ExaminationId
+WHERE 
+ExaminationDate BETWEEN '2010-01-01' AND '2024-01-25'
+AND (__COND__)
+"""
+
+q_template_cond = """LOWER(rp.ReportText) LIKE LOWER('__VAL__')\n"""
+
+q = ""
+for i, key_val in enumerate(keywords.items()):
+    if i > 0:
+        q += "\nUNION\n"
+
+    q_cond = ""
+    for j, val in enumerate(key_val[1]):
+        if j > 0:
+            q_cond += " OR "
+        
+        q_cond += q_template_cond.replace("__VAL__", val)
+
+    q += q_template_main.replace("__KEY__", key_val[0]).replace("__COND__", q_cond)
+
+
+print(q)
+
+
+
+
+
+# COMMAND ----------
+
+tmp_df = spark.sql(q)
+
+display(tmp_df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT COUNT(DISTINCT REPORTId) FROM 4_prod.raw.pacs_reports AS r
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+# MAGIC ON r.ReportId = er.ExaminationReportReportId
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinations AS e
+# MAGIC ON er.ExaminationReportExaminationId = e.ExaminationId
+# MAGIC INNER JOIN breast_cancer_patients AS bcp
+# MAGIC ON e.ExaminationPatientId = bcp.PatientId
+# MAGIC WHERE LOWER(ReportText) LIKE '%bi-rads%' 
+# MAGIC LIMIT 10
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC SELECT COUNT(DISTINCT REPORTId) FROM 4_prod.raw.pacs_reports AS r
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinationreports AS er
+# MAGIC ON r.ReportId = er.ExaminationReportReportId
+# MAGIC INNER JOIN 4_prod.raw.pacs_examinations AS e
+# MAGIC ON er.ExaminationReportExaminationId = e.ExaminationId
+# MAGIC INNER JOIN breast_cancer_patients AS bcp
+# MAGIC ON e.ExaminationPatientId = bcp.PatientId
+# MAGIC WHERE LOWER(ReportText) LIKE '%focus%' 
 # MAGIC LIMIT 10
