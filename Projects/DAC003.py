4479d81cd96a6e9246d57547c444bb90b4fc8df8
ev-h <ev-h>
2025-06-24 13:50:00 +0000

    Updated DAC003

--- Projects/DAC003.py
+++ Projects/DAC003.py
@@ -46,7 +46,227 @@
 
 # COMMAND ----------
 
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS 5_projects.dac003_breastonestop.breast_patient_order_list_202406;
+# MAGIC
+# MAGIC
+# MAGIC CREATE TABLE 5_projects.dac003_breastonestop.breast_patient_order_list_202406(
+# MAGIC     PERSON_ID BIGINT,
+# MAGIC     ORDER_ID BIGINT,
+# MAGIC     CATALOG_CD BIGINT,
+# MAGIC     ORDER_MNEMONIC VARCHAR(400),
+# MAGIC     ORDER_DT_TM TIMESTAMP,
+# MAGIC     SRC_TABLE VARCHAR(100)
+# MAGIC );
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC WITH cd AS (
+# MAGIC     SELECT *
+# MAGIC     FROM (
+# MAGIC         VALUES
+# MAGIC         (6180043), -- US Breast Rt
+# MAGIC         (6181432), -- US Guided core biopsy breast Lt
+# MAGIC         (6182217), -- US Breast Lt
+# MAGIC         (6183363), -- US Guided core biopsy breast Rt
+# MAGIC         (6183590) -- US Breast Both
+# MAGIC     ) AS tmp(CATALOG_CD)
+# MAGIC )
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_order_list_202406
+# MAGIC SELECT
+# MAGIC     PERSON_ID,
+# MAGIC     ORDER_ID,
+# MAGIC     o.CATALOG_CD,
+# MAGIC     ORDER_MNEMONIC,
+# MAGIC     o.ORIG_ORDER_DT_TM,
+# MAGIC     'MILL_DIR_ORDERS'
+# MAGIC FROM 4_prod.raw.MILL_ORDERS AS o
+# MAGIC INNER JOIN cd
+# MAGIC     ON o.CATALOG_CD = cd.CATALOG_CD
+# MAGIC ;
+# MAGIC
+# MAGIC
+# MAGIC /**
+# MAGIC WITH cd AS (
+# MAGIC     SELECT *
+# MAGIC     FROM (
+# MAGIC         VALUES
+# MAGIC         ('US Breast Rt'),
+# MAGIC         ('US Guided core biopsy breast Lt'),
+# MAGIC         ('US Breast Lt'),
+# MAGIC         ('US Guided core biopsy breast Rt'),
+# MAGIC         ('US Breast Both')
+# MAGIC     ) AS tmp(order_txt)
+# MAGIC )
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_order_list_202406
+# MAGIC SELECT
+# MAGIC     PERSON_ID,
+# MAGIC     ORDER_ID,
+# MAGIC     0 AS catalog_cd,
+# MAGIC     ORDER_MNEM_TXT,
+# MAGIC     ORDER_DT_TM,
+# MAGIC     'PI_CDE_ORDER' AS src_table
+# MAGIC FROM 4_prod.raw.PI_CDE_ORDER AS o
+# MAGIC INNER JOIN cd
+# MAGIC     ON LOWER(LTRIM(RTRIM(o.ORDER_MNEM_TXT))) = LOWER(LTRIM(RTRIM(cd.order_txt)))
+# MAGIC ;
+# MAGIC
+# MAGIC **/
+# MAGIC
+# MAGIC SELECT 
+# MAGIC     ORDER_MNEMONIC, 
+# MAGIC     COUNT(DISTINCT PERSON_ID) AS patient_count
+# MAGIC FROM 5_projects.dac003_breastonestop.breast_patient_order_list_202406
+# MAGIC WHERE order_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
+# MAGIC GROUP BY ORDER_MNEMONIC
+# MAGIC
+# MAGIC UNION ALL
+# MAGIC
+# MAGIC SELECT 
+# MAGIC     'Total num of patients' AS ORDER_MNEMONIC, 
+# MAGIC     COUNT(DISTINCT PERSON_ID) AS patient_count
+# MAGIC FROM 5_projects.dac003_breastonestop.breast_patient_order_list_202406
+# MAGIC WHERE order_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
 
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS 5_projects.dac003_breastonestop.breast_patient_opa_list_202406;
+# MAGIC
+# MAGIC
+# MAGIC CREATE TABLE 5_projects.dac003_breastonestop.breast_patient_opa_list_202406(
+# MAGIC     person_id BIGINT,
+# MAGIC     src_table VARCHAR(200),
+# MAGIC     src_id VARCHAR(200),
+# MAGIC     src_id_col VARCHAR(100),
+# MAGIC     src_event_dt_tm TIMESTAMP,
+# MAGIC     src_code_value VARCHAR(100)
+# MAGIC );
+# MAGIC
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_opa_list_202406
+# MAGIC SELECT
+# MAGIC   ce.person_id,
+# MAGIC   'MILL_SCH_APPT',
+# MAGIC   NULL,
+# MAGIC   NULL,
+# MAGIC   COALESCE(ce.EVENT_START_DT_TM, ce.EVENT_END_DT_TM),
+# MAGIC   ce.EVENT_CD
+# MAGIC FROM 4_prod.raw.mill_sch_appt AS sa 
+# MAGIC INNER JOIN 4_prod.raw.mill_clinical_event AS ce 
+# MAGIC ON ce.EVENT_ID = sa.SCH_EVENT_ID
+# MAGIC WHERE 
+# MAGIC   sa.DESCRIPTION ILIKE '%breast%'
+# MAGIC   AND COALESCE(ce.EVENT_START_DT_TM, ce.EVENT_END_DT_TM) BETWEEN '2010-01-01' AND '2024-01-25';
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS 5_projects.dac003_breastonestop.breast_patient_list_202406;
+# MAGIC
+# MAGIC CREATE TABLE 5_projects.dac003_breastonestop.breast_patient_list_202406(
+# MAGIC     person_id BIGINT,
+# MAGIC     ehr_code_type VARCHAR(100),
+# MAGIC     ehr_code_value VARCHAR(100),
+# MAGIC     src_table VARCHAR(200),
+# MAGIC     src_id VARCHAR(200),
+# MAGIC     src_id_col VARCHAR(100),
+# MAGIC     src_event_dt_tm TIMESTAMP,
+# MAGIC     src_code_value VARCHAR(100)
+# MAGIC );
+# MAGIC
+# MAGIC /**
+# MAGIC WITH icd AS (
+# MAGIC     SELECT *
+# MAGIC     FROM 5_projects.dac003_breastonestop.breast_patient_codes
+# MAGIC     WHERE code_type LIKE 'ICD-10%'
+# MAGIC )
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_list_202406
+# MAGIC SELECT
+# MAGIC     e.person_id,
+# MAGIC     c.code_type AS ehr_code_type,
+# MAGIC     c.code_value,
+# MAGIC     'PI_CDE_DIAGNOSIS' AS src_table,
+# MAGIC     p.diagnosis_id,
+# MAGIC     'DIAGNOSIS_ID' AS src_id_col,
+# MAGIC     COALESCE(p.diagnosis_dt_tm, e.BEG_EFFECTIVE_DT_TM),
+# MAGIC     p.CONCEPT_CKI_IDENT
+# MAGIC FROM BH_DATAWAREHOUSE.dbo.PI_CDE_DIAGNOSIS AS p
+# MAGIC INNER JOIN icd AS c
+# MAGIC     ON p.CONCEPT_CKI_IDENT LIKE 'ICD10WHO!'+LTRIM(RTRIM(c.code_value))+'%'
+# MAGIC INNER JOIN BH_DATAWAREHOUSE.dbo.PI_CDE_ENCOUNTER AS pe
+# MAGIC     ON p.ENCNTR_ID = pe.ENCNTR_ID
+# MAGIC INNER JOIN BH_DATAWAREHOUSE.dbo.MILL_DIR_ENCOUNTER AS e
+# MAGIC     ON p.ENCNTR_ID = e.ENCNTR_ID AND pe.PERSON_ID = e.PERSON_ID
+# MAGIC WHERE
+# MAGIC     p.ACTIVE_IND = 1
+# MAGIC     AND e.ACTIVE_IND = 1
+# MAGIC     AND e.BEG_EFFECTIVE_DT_TM IS NOT NULL
+# MAGIC
+# MAGIC **/
+# MAGIC
+# MAGIC WITH opcs AS (
+# MAGIC     SELECT *
+# MAGIC     FROM 5_projects.dac003_breastonestop.breast_patient_codes
+# MAGIC     WHERE code_type ILIKE 'OPCS-4%'
+# MAGIC )
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_list_202406
+# MAGIC SELECT
+# MAGIC     e.person_id,
+# MAGIC     c.code_type AS ehr_code_type,
+# MAGIC     c.code_value,
+# MAGIC     'MILL_DIR_PROCEDURE' AS src_table,
+# MAGIC     p.procedure_id,
+# MAGIC     'PROCEDURE_ID' AS src_id_col,
+# MAGIC     e.BEG_EFFECTIVE_DT_TM,
+# MAGIC     COALESCE(n.CONCEPT_CKI, n.SOURCE_IDENTIFIER)
+# MAGIC FROM 4_prod.raw.MILL_PROCEDURE AS p
+# MAGIC INNER JOIN 3_lookup.mill.MILL_NOMENCLATURE AS n
+# MAGIC     ON p.NOMENCLATURE_ID = n.NOMENCLATURE_ID
+# MAGIC INNER JOIN opcs AS c
+# MAGIC     ON LTRIM(RTRIM(SOURCE_IDENTIFIER)) ILIKE CONCAT(LTRIM(RTRIM(c.code_value)),'%')
+# MAGIC INNER JOIN 4_prod.raw.MILL_ENCOUNTER AS e
+# MAGIC     ON p.ENCNTR_ID = e.ENCNTR_ID
+# MAGIC WHERE 
+# MAGIC     SOURCE_VOCABULARY_CD = 685812 -- OPCS4
+# MAGIC     AND e.BEG_EFFECTIVE_DT_TM IS NOT NULL
+# MAGIC
+# MAGIC ;
+# MAGIC
+# MAGIC WITH icd AS (
+# MAGIC     SELECT *
+# MAGIC     FROM 5_projects.dac003_breastonestop.breast_patient_codes
+# MAGIC     WHERE code_type ILIKE 'ICD-10%'
+# MAGIC )
+# MAGIC INSERT INTO 5_projects.dac003_breastonestop.breast_patient_list_202406
+# MAGIC SELECT
+# MAGIC     e.person_id,
+# MAGIC     c.code_type AS ehr_code_type,
+# MAGIC     c.code_value,
+# MAGIC     'MILL_DIR_DIAGNOSIS' AS src_table,
+# MAGIC     d.diagnosis_id,
+# MAGIC     'DIAGNOSIS_ID' AS src_id_col,
+# MAGIC     e.BEG_EFFECTIVE_DT_TM,
+# MAGIC     COALESCE(n.CONCEPT_CKI, n.SOURCE_IDENTIFIER)
+# MAGIC FROM 4_prod.raw.MILL_DIAGNOSIS AS d
+# MAGIC INNER JOIN 3_lookup.mill.MILL_NOMENCLATURE AS n
+# MAGIC     ON d.NOMENCLATURE_ID = n.NOMENCLATURE_ID
+# MAGIC INNER JOIN 5_projects.dac003_breastonestop.breast_patient_codes AS c
+# MAGIC     ON LTRIM(RTRIM(n.SOURCE_IDENTIFIER)) ILIKE CONCAT(LTRIM(RTRIM(c.code_value)),'%')
+# MAGIC INNER JOIN 4_prod.raw.MILL_ENCOUNTER AS e
+# MAGIC     ON d.ENCNTR_ID = e.ENCNTR_ID
+# MAGIC WHERE 
+# MAGIC     c.code_type = 'ICD-10_prefix' 
+# MAGIC     AND CONCEPT_CKI ILIKE 'ICD10WHO%'
+# MAGIC     AND e.BEG_EFFECTIVE_DT_TM IS NOT NULL
+# MAGIC ;
+# MAGIC
+# MAGIC SELECT 
+# MAGIC     COUNT(DISTINCT PERSON_ID) AS person_count
+# MAGIC FROM 5_projects.dac003_breastonestop.breast_patient_list_202406
+# MAGIC WHERE src_event_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
+# MAGIC
 
 # COMMAND ----------
 
@@ -56,34 +276,39 @@ project_identifier = 'dac003'
 
 rde_tables = ['rde_aliases', 'rde_all_procedures', 'rde_allergydetails', 'rde_apc_diagnosis', 'rde_apc_opcs', 'rde_ariapharmacy', 'rde_blobdataset', 'rde_cds_apc', 'rde_cds_opa', 'rde_critactivity', 'rde_critopcs', 'rde_critperiod', 'rde_emergencyd', 'rde_encounter', 'rde_family_history', 'rde_iqemo', 'rde_measurements', 'rde_medadmin', 'rde_op_diagnosis', 'rde_opa_opcs', 'rde_pathology', 'rde_patient_demographics', 'rde_pc_diagnosis', 'rde_pc_problems', 'rde_pc_procedures', 'rde_pharmacyorders', 'rde_radiology', 'rde_raw_pathology']
 
-max_ig_risk = 4
-max_ig_severity = 3
+max_ig_risk = 3
+max_ig_severity = 2
 columns_to_exclude = ['ADC_UPDT']
 
 cohort_sql = f"""
 CREATE OR REPLACE VIEW 6_mgmt.cohorts.dac003 AS
-WITH matched_aliases AS (
-  SELECT DISTINCT l.Identifier, pa.PERSON_ID
-  FROM 6_mgmt.cohort_lookup.dac003_lookup l
-  JOIN 4_prod.raw.mill_person_alias pa
-  ON REGEXP_REPLACE(l.Identifier, '[ -]', '') = REGEXP_REPLACE(pa.ALIAS, '[ -]', '')
-),
-matched_persons AS (
-  SELECT DISTINCT 
-    l.Identifier,
-    p.PERSON_ID
-  FROM 6_mgmt.cohort_lookup.dac003_lookup l
-  JOIN 4_prod.raw.mill_person p
-  ON UPPER(l.Name) = CONCAT(p.NAME_FIRST_KEY, ' ', p.NAME_LAST_KEY)
-  AND TO_DATE(l.dob, 'dd/MM/yyyy') = DATE(p.BIRTH_DT_TM)
+WITH cte AS (
+
+    -- Extracted from MILL_SCH_APPT (instead of PI_CDE_OP_ATTENDANCE)
+    SELECT DISTINCT PERSON_ID
+    FROM 5_projects.dac003_breastonestop.breast_patient_opa_list_202406
+    WHERE src_event_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
+
+    UNION
+
+    -- Extracted from MILL_DIR_DIAGNOSIS, MILL_DIR_PROCEDURE
+    SELECT DISTINCT PERSON_ID
+    FROM 5_projects.dac003_breastonestop.breast_patient_list_202406
+    WHERE src_event_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
+
+    UNION
+
+    -- Extracted from MILL_DIR_ORDERS
+    SELECT DISTINCT PERSON_ID
+    FROM 5_projects.dac003_breastonestop.breast_patient_order_list_202406
+    WHERE order_dt_tm BETWEEN '2010-01-01' AND '2024-01-25'
+
+
+
+
 )
-SELECT DISTINCT
-  COALESCE(ma.PERSON_ID, mp.PERSON_ID) as PERSON_ID,
-  CAST(NULL as STRING) as subcohort
-FROM 6_mgmt.cohort_lookup.dac003_lookup l
-LEFT JOIN matched_aliases ma ON l.Identifier = ma.Identifier
-LEFT JOIN matched_persons mp ON l.Identifier = mp.Identifier
-WHERE COALESCE(ma.PERSON_ID, mp.PERSON_ID) IS NOT NULL;
+SELECT DISTINCT PERSON_ID
+FROM cte;
 """
 spark.sql(cohort_sql)
 
